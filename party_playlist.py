6d8a994407a9768a5c0809a3fcc5e2e73313aa83
timeyyy <timeyyy_da_man@hotmail.com>
2015-01-22 13:45:18 +0100

    what

--- party_playlist.py
+++ party_playlist.py
@@ -1,17 +1,6 @@
 import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-
-while True:
-  #take a reading
-  input = GPIO.input(17)
-  #if the last reading was low and this one high, print
-  if ((not prev_input) and input):
-    print("Button pressed")
-  #update previous input
-  prev_input = input
-  #slight pause to debounce
-  time.sleep(0.05)
 import _thread as thread
 from queue import Queue
 
