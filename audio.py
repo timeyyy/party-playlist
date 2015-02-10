47414b61189ed453fbd228c290f20ab57f9e749a
timeyyy <timeyyy_da_man@hotmail.com>
2015-02-10 15:22:57 -0700

    ass

--- audio.py
+++ audio.py
@@ -5,8 +5,6 @@ import wave
 
 from pydub import AudioSegment
 
-
-
 FILE = 'waves1.wav'
 FILE_SIZE = os.path.getsize(FILE)
 CHUNK_SIZE = 1024
@@ -14,22 +12,21 @@ FORMAT = pyaudio.paInt16
 RATE = 44100
 
 chunk = 1024
-
-song = AudioSegment.from_wav(FILE)
-
-#~ wf = wave.open(FILE, 'rb')
-#~ p = pyaudio.PyAudio()
-#~ 
-#~ stream = p.open(
-    #~ format = p.get_format_from_width(wf.getsampwidth()),
-    #~ channels = wf.getnchannels(),
-    #~ rate = wf.getframerate(),
-    #~ output = True)
-#~ data = wf.readframes(chunk)
-#~ 
-#~ while data != '':
-    #~ stream.write(data)
-    #~ data = wf.readframes(chunk)
-#~ 
-#~ stream.close()
-#~ p.terminate()
+def run():
+	song = AudioSegment.from_mp3()
+	print(song.__dict__)
+def play():
+	wf = wave.open(FILE, 'rb')
+	p = pyaudio.PyAudio()
+	stream = p.open(
+		format = p.get_format_from_width(wf.getsampwidth()),
+		channels = wf.getnchannels(),
+		rate = wf.getframerate(),
+		output = True)
+	data = wf.readframes(chunk)
+ 
+	while data != '':
+		stream.write(data)
+		data = wf.readframes(chunk) 
+	stream.close()
+	p.terminate()
